a10de3b6cee1611a59505cf9922d6e9f368c98f8
Matias Lavik <matias.lavik@hotmail.com>
2020-01-01 21:55:44 +0100

    Removed *.*
    Removed *.*. This should make dataset import work on Linux.

--- Assets/Editor/VolumeRendererEditorFunctions.cs
+++ Assets/Editor/VolumeRendererEditorFunctions.cs
@@ -7,7 +7,7 @@ public class VolumeRendererEditorFunctions
     [MenuItem("Volume Rendering/Load dataset")]
     static void ShowWindow()
     {
-        string file = EditorUtility.OpenFilePanel("Select a dataset to load", "DataFiles", "*.*");
+        string file = EditorUtility.OpenFilePanel("Select a dataset to load", "DataFiles", "");
         if(File.Exists(file))
         {
             DatasetImporterEditorWindow wnd = (DatasetImporterEditorWindow)EditorWindow.GetWindow(typeof(DatasetImporterEditorWindow));
