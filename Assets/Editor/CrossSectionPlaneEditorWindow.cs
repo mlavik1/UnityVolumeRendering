6369c346d58dbbdd00732fd62c5c6387be326a7a
Matias Lavik <mlavik123@gmail.com>
2020-01-31 20:30:07 +0100

    Added support for spawning the cross section tool from the menu.

---
+++ Assets/Editor/CrossSectionPlaneEditorWindow.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CrossSectionPlaneEditorWindow : EditorWindow
+{
+    [MenuItem("Volume Rendering/Cross section")]
+    static void OnMenuItemClick()
+    {
+        VolumeRenderedObject[] objects = GameObject.FindObjectsOfType<VolumeRenderedObject>();
+        if (objects.Length == 1)
+            SpawnCrossSectionPlane(objects[0]);
+        else
+        {
+            CrossSectionPlaneEditorWindow wnd = new CrossSectionPlaneEditorWindow();
+            wnd.Show();
+        }
+    }
+
+    private static void SpawnCrossSectionPlane(VolumeRenderedObject volobj)
+    {
+        GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        quad.transform.rotation = Quaternion.Euler(270.0f, 0.0f, 0.0f);
+        SlicingPlaneAnyDirection csplane = quad.gameObject.AddComponent<SlicingPlaneAnyDirection>();
+        csplane.mat = volobj.GetComponent<MeshRenderer>().sharedMaterial;
+        csplane.volumeTransform = volobj.transform;
+        quad.transform.position = volobj.transform.position;
+
+        Selection.objects = new Object[]{ quad };
+    }
+
+    private void OnGUI()
+    {
+        VolumeRenderedObject[] spawnedObjects = GameObject.FindObjectsOfType<VolumeRenderedObject>();
+        if(spawnedObjects.Length == 0)
+        {
+            EditorGUILayout.LabelField("Please load a dataset first.");
+        }
+        else
+        {
+            foreach(VolumeRenderedObject volobj in spawnedObjects)
+            {
+                if (GUILayout.Button(volobj.gameObject.name))
+                    SpawnCrossSectionPlane(volobj);
+            }
+        }
+    }
+}
