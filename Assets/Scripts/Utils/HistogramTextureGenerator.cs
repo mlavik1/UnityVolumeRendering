d2f3086da9ac9fff14692ef07f95029f08146f1b
Matias Lavik <mlavik123@gmail.com>
2020-02-20 09:33:11 +0100

    Fix for negative data values not working properly

--- Assets/Scripts/Utils/HistogramTextureGenerator.cs
+++ Assets/Scripts/Utils/HistogramTextureGenerator.cs
@@ -6,7 +6,10 @@ namespace UnityVolumeRendering
     {
         public static Texture2D GenerateHistogramTexture(VolumeDataset dataset)
         {
-            int numSamples = dataset.GetMaxDataValue() + 1;
+            int minValue = dataset.GetMinDataValue();
+            int maxValue = dataset.GetMaxDataValue();
+
+            int numSamples = maxValue - minValue + 1;
             int[] values = new int[numSamples];
             Color[] cols = new Color[numSamples];
             Texture2D texture = new Texture2D(numSamples, 1, TextureFormat.RGBAFloat, false);
@@ -14,8 +17,9 @@ namespace UnityVolumeRendering
             int maxFreq = 0;
             for (int iData = 0; iData < dataset.data.Length; iData++)
             {
-                values[dataset.data[iData]] += 1;
-                maxFreq = System.Math.Max(values[dataset.data[iData]], maxFreq);
+                int dataValue = dataset.data[iData] - minValue;
+                values[dataValue] += 1;
+                maxFreq = System.Math.Max(values[dataValue], maxFreq);
             }
 
             for (int iSample = 0; iSample < numSamples; iSample++)
