5c2a144f54c56c18dea8e4b83c0f2bcffbddf914
jasonks2 <85245498+jasonks2@users.noreply.github.com>
2021-09-14 11:08:30 -0500

    Update HistogramTextureGenerator.cs

--- Assets/Scripts/Utils/HistogramTextureGenerator.cs
+++ Assets/Scripts/Utils/HistogramTextureGenerator.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using UnityEngine;
 
 namespace UnityVolumeRendering
@@ -17,6 +17,7 @@ namespace UnityVolumeRendering
         /// <returns></returns>
         public static Texture2D GenerateHistogramTexture(VolumeDataset dataset)
         {
+
             int minValue = dataset.GetMinDataValue();
             int maxValue = dataset.GetMaxDataValue();
             int numValues = maxValue - minValue + 1;
@@ -29,6 +30,7 @@ namespace UnityVolumeRendering
             Texture2D texture = new Texture2D(numSamples, 1, TextureFormat.RGBAFloat, false);
 
             int maxFreq = 0;
+
             for (int iData = 0; iData < dataset.data.Length; iData++)
             {
                 int dataValue = dataset.data[iData];
@@ -58,6 +60,56 @@ namespace UnityVolumeRendering
         /// <returns></returns>
         public static Texture2D GenerateHistogramTextureOnGPU(VolumeDataset dataset)
         {
+
+            DatasetType datasetType = DatasetImporterUtility.GetDatasetType(dataset.filePath);
+
+            if (datasetType == DatasetType.PARCHG)
+            {
+                            double actualBound = dataset.GetMaxDataValueDouble() - dataset.GetMinDataValueDouble() + 1;
+                            int numValues = System.Convert.ToInt16(dataset.GetMaxDataValueDouble() - dataset.GetMinDataValueDouble() + 1); // removed +1
+                            int sampleCount = System.Math.Min(numValues, 256);
+
+                        ComputeShader computeHistogram = Resources.Load("ComputeHistogram") as ComputeShader;
+                     int handleInitialize = computeHistogram.FindKernel("HistogramInitialize");
+                 int handleMain = computeHistogram.FindKernel("HistogramMain");
+
+                 ComputeBuffer histogramBuffer = new ComputeBuffer(sampleCount, sizeof(uint) * 1);
+                 uint[] histogramData = new uint[sampleCount];
+            Color32 [] histogramCols = new Color32[sampleCount];
+
+            Texture3D dataTexture = dataset.GetDataTexture();
+
+            if (handleInitialize < 0 || handleMain < 0)
+            {
+                Debug.LogError("Histogram compute shader initialization failed.");
+            }
+
+            computeHistogram.SetFloat("ValueRange", (float)(actualBound - 1));
+            computeHistogram.SetTexture(handleMain, "VolumeTexture", dataTexture);
+            computeHistogram.SetBuffer(handleMain, "HistogramBuffer", histogramBuffer);
+            computeHistogram.SetBuffer(handleInitialize, "HistogramBuffer", histogramBuffer);
+
+            computeHistogram.Dispatch(handleInitialize, sampleCount / 8, 1, 1);
+            computeHistogram.Dispatch(handleMain, (dataTexture.width + 7) / 8, (dataTexture.height + 7) / 8, (dataTexture.depth + 7) / 8);
+
+            histogramBuffer.GetData(histogramData);
+
+            int maxValue = (int)histogramData.Max();
+            
+            Texture2D texture = new Texture2D(sampleCount, 1, TextureFormat.RGBA32, false);
+            for (int iSample = 0; iSample < sampleCount; iSample++)
+            {
+                histogramCols[iSample] = new Color(Mathf.Log10((float)histogramData[iSample]) / Mathf.Log10((float)maxValue), 0.0f, 0.0f, 1.0f);
+            }
+
+            texture.SetPixels32(histogramCols);
+            texture.Apply();
+
+            return texture;
+            }
+
+            else
+            {
             int numValues = dataset.GetMaxDataValue() - dataset.GetMinDataValue() + 1;
             int sampleCount = System.Math.Min(numValues, 256);
 
@@ -98,6 +150,7 @@ namespace UnityVolumeRendering
             texture.Apply();
 
             return texture;
+            }
         }
 
         /// <summary>
@@ -110,8 +163,77 @@ namespace UnityVolumeRendering
         /// <returns></returns>
         public static Texture2D Generate2DHistogramTexture(VolumeDataset dataset)
         {
+
+            DatasetType datasetType = DatasetImporterUtility.GetDatasetType(dataset.filePath);
+            if (datasetType == DatasetType.PARCHG)
+            {
+                int numSamples = System.Convert.ToInt16(dataset.GetMaxDataValueDouble() + 1);
+                int numGradientSamples = 256;
+
+                Color[] cols = new Color[numSamples * numGradientSamples];
+                Texture2D texture = new Texture2D(numSamples, numGradientSamples, TextureFormat.RGBAFloat, false);
+
+                Debug.Log(dataset.GetMaxDataValueDouble() + " " + dataset.GetMinDataValueDouble());
+                Debug.Log(numSamples * numGradientSamples);
+
+                for (int iCol = 0; iCol < cols.Length; iCol++)
+                    cols[iCol] = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+
+                        double maxRange = dataset.GetMaxDataValueDouble() - dataset.GetMinDataValueDouble();
+                        const float maxNormalisedMagnitude = 1.75f; // sqrt(1^2 + 1^2 + 1^2) = swrt(3) = a bit less than 1.75
+
+                     for (int x = 1; x < dataset.dimX - 1; x++)
+                        {
+                            for (int y = 1; y < dataset.dimY - 1; y++)
+                            {
+                             for (int z = 1; z < dataset.dimZ - 1; z++)
+                                {
+                                int iData = x + y * dataset.dimX + z * (dataset.dimX * dataset.dimY);
+
+
+                                double density = dataset.dataGrid[iData];
+
+                                 double x1 = dataset.dataGrid[(x + 1) + y * dataset.dimX + z * (dataset.dimX * dataset.dimY)];
+                                    double x2 = dataset.dataGrid[(x - 1) + y * dataset.dimX + z * (dataset.dimX * dataset.dimY)];
+
+                                    double y1 = dataset.dataGrid[x + (y + 1) * dataset.dimX + z * (dataset.dimX * dataset.dimY)];
+                                    double y2 = dataset.dataGrid[x + (y - 1) * dataset.dimX + z * (dataset.dimX * dataset.dimY)];
+
+                                    double z1 = dataset.dataGrid[x + y * dataset.dimX + (z + 1) * (dataset.dimX * dataset.dimY)];
+                                     double z2 = dataset.dataGrid[x + y * dataset.dimX + (z - 1) * (dataset.dimX * dataset.dimY)];
+
+                                Vector3 grad = new Vector3((float)(x2 - x1) / (float)maxRange, (float)(y2 - y1) / (float)maxRange, (float)(z2 - z1) / (float)maxRange);
+                                      cols[(int)(density + (grad.magnitude * numGradientSamples / maxNormalisedMagnitude) * numSamples)] = Color.white;
+                    /* 
+
+                        double x1 = dataGrid[Math.Min(x + 1, dimX - 1) + y * dimX + z * (dimX * dimY)] - minValue;
+                        double x2 = dataGrid[Math.Max(x - 1, 0) + y * dimX + z * (dimX * dimY)] - minValue;
+                        
+                        double y1 = dataGrid[x + Math.Min(y + 1, dimY - 1) * dimX + z * (dimX * dimY)] - minValue;
+                        double y2 = dataGrid[x + Math.Max(y - 1, 0) * dimX + z * (dimX * dimY)] - minValue;
+
+                        double z1 = dataGrid[x + y * dimX + Math.Min(z + 1, dimZ - 1) * (dimX * dimY)] - minValue;
+                        double z2 = dataGrid[x + y * dimX + Math.Max(z - 1, 0) * (dimX * dimY)] - minValue;
+                        Vector3 grad = new Vector3((float)(x2 - x1) / (float)maxRange, (float)(y2 - y1) / (float)maxRange, (float)(z2 - z1) / (float)maxRange);
+
+                        gradColors[iData] = new Color(grad.x, grad.y, grad.z, (float)(dataGrid[iData] - (float)minValue) / (float)maxRange);*/
+                    
+                    }
+                }
+            }
+
+            texture.SetPixels(cols);
+            texture.Apply();
+
+            return texture;
+            }
+            
+            else 
+            {
             int numSamples = dataset.GetMaxDataValue() + 1;
             int numGradientSamples = 256;
+
+            
             Color[] cols = new Color[numSamples * numGradientSamples];
             Texture2D texture = new Texture2D(numSamples, numGradientSamples, TextureFormat.RGBAFloat, false);
 
@@ -147,6 +269,7 @@ namespace UnityVolumeRendering
             texture.Apply();
 
             return texture;
+            }
         }
     }
 }
