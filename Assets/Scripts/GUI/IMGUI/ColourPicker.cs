0cd3239d112bcfdf92a89ffe939aad354dbad304
Matias Lavik <matias.lavik@catenda.no>
2022-09-18 10:59:56 +0200

    Colour picker

--- Assets/Scripts/GUI/IMGUI/ColourPicker.cs
+++ Assets/Scripts/GUI/IMGUI/ColourPicker.cs
@@ -6,23 +6,34 @@ namespace UnityVolumeRendering
     {
         private static ColourPickerPopup colourPicker = null; // TODO: Not static?
 
+        private static Texture2D previewTexture = null;
+        private static GUIStyle previewStyle = new GUIStyle();
+
         public static Color ColourField(Rect rect, Color colour)
         {
-            Color oldColour = GUI.backgroundColor;
-            GUI.backgroundColor = colour;
-            if (GUI.Button(rect, ""))
+            if (previewTexture == null)
+            {
+                previewTexture = new Texture2D(2, 2, TextureFormat.RGBAFloat, false);
+            }
+
+            if (colourPicker)
+                colour = colourPicker.GetColour();
+            
+            Color[] previewCols = { colour, colour, colour, colour };
+            previewTexture.SetPixels(previewCols);
+            previewTexture.Apply();
+            previewStyle.normal.background = previewTexture;
+            previewStyle.alignment = TextAnchor.MiddleCenter;
+            if (GUI.Button(rect, "Colour", previewStyle))
             {
                 if (colourPicker == null)
                 {
                     GameObject obj = new GameObject();
                     colourPicker = obj.AddComponent<ColourPickerPopup>();
-                    colourPicker.callback = (Color col) =>
-                    {
-                        // ???
-                    };
                 }
+                colourPicker.SetColour(colour);
             }
-            GUI.backgroundColor = oldColour;
+
             if (colourPicker)
                 return colourPicker.GetColour();
             else
