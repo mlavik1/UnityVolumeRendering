0f021c52541094b8368f913ee50eaa0994ee46f9
Matias Lavik <matias.lavik@catenda.no>
2023-06-06 21:08:43 +0200

    Add missing namespaces

--- Assets/Scripts/VolumeObject/CutoutSphere.cs
+++ Assets/Scripts/VolumeObject/CutoutSphere.cs
@@ -1,58 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityVolumeRendering;
+﻿using UnityEngine;
 
-
-/// <summary>
-/// Cutout sphere.
-/// Used for cutting a model (cutout view).
-/// </summary>
-[ExecuteInEditMode]
-public class CutoutSphere : MonoBehaviour, CrossSectionObject
+namespace UnityVolumeRendering
 {
-    [SerializeField]
-    private VolumeRenderedObject targetObject;
-    [field: SerializeField] public CutoutType CutoutType { get; set; } = CutoutType.Exclusive;
-
-    public CrossSectionType GetCrossSectionType()
+    /// <summary>
+    /// Cutout sphere.
+    /// Used for cutting a model (cutout view).
+    /// </summary>
+    [ExecuteInEditMode]
+    public class CutoutSphere : MonoBehaviour, CrossSectionObject
     {
-        switch (CutoutType)
+        [SerializeField]
+        private VolumeRenderedObject targetObject;
+        [field: SerializeField] public CutoutType CutoutType { get; set; } = CutoutType.Exclusive;
+
+        public CrossSectionType GetCrossSectionType()
         {
-            case CutoutType.Inclusive:
-                return CrossSectionType.SphereInclusive;
-            case CutoutType.Exclusive:
-                return CrossSectionType.SphereExclusive;
-            default:
-                throw new System.NotImplementedException();
+            switch (CutoutType)
+            {
+                case CutoutType.Inclusive:
+                    return CrossSectionType.SphereInclusive;
+                case CutoutType.Exclusive:
+                    return CrossSectionType.SphereExclusive;
+                default:
+                    throw new System.NotImplementedException();
+            }
         }
-    }
 
-    public Matrix4x4 GetMatrix()
-    {
-        return transform.worldToLocalMatrix * targetObject.volumeContainerObject.transform.localToWorldMatrix;
-    }
+        public Matrix4x4 GetMatrix()
+        {
+            return transform.worldToLocalMatrix * targetObject.volumeContainerObject.transform.localToWorldMatrix;
+        }
 
-    private void OnEnable()
-    {
-        if (targetObject != null)
-            targetObject.GetCrossSectionManager().AddCrossSectionObject(this);
-    }
+        private void OnEnable()
+        {
+            if (targetObject != null)
+                targetObject.GetCrossSectionManager().AddCrossSectionObject(this);
+        }
 
-    private void OnDisable()
-    {
-        if (targetObject != null)
-            targetObject.GetCrossSectionManager().RemoveCrossSectionObject(this);
-    }
+        private void OnDisable()
+        {
+            if (targetObject != null)
+                targetObject.GetCrossSectionManager().RemoveCrossSectionObject(this);
+        }
 
-    public void SetTargetObject(VolumeRenderedObject target)
-    {
-        if (this.enabled && targetObject != null)
-            targetObject.GetCrossSectionManager().RemoveCrossSectionObject(this);
+        public void SetTargetObject(VolumeRenderedObject target)
+        {
+            if (this.enabled && targetObject != null)
+                targetObject.GetCrossSectionManager().RemoveCrossSectionObject(this);
 
-        targetObject = target;
+            targetObject = target;
 
-        if (this.enabled && targetObject != null)
-            targetObject.GetCrossSectionManager().AddCrossSectionObject(this);
+            if (this.enabled && targetObject != null)
+                targetObject.GetCrossSectionManager().AddCrossSectionObject(this);
+        }
     }
 }
