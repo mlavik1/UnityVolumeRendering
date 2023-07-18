97cb28407c06eb331bedb7195ef14c6d553d63a0
Matias Lavik <mlavik123@gmail.com>
2023-07-18 13:05:48 +0200

    Added scripted importer for TF

---
+++ Assets/Editor/ScriptedImporters/TransferFunctionScriptedImporter.cs
@@ -0,0 +1,33 @@
+﻿#if UNITY_2020_2_OR_NEWER
+using UnityEngine;
+using UnityEditor.AssetImporters;
+
+namespace UnityVolumeRendering
+{
+    /// <summary>
+    /// ScriptedImporter for raw datasets.
+    /// Allows you to import datasets as assets by dragging them into the project view.
+    /// Imported dataset assets can be dragged-and-dropped into the scene view/hierarchy, or spawned from code.
+    /// </summary>
+    [ScriptedImporter(1, "tf")]
+    public class TransferFunctionScriptedImporter : ScriptedImporter
+    {
+        public override void OnImportAsset(AssetImportContext ctx)
+        {
+            string fileToImport = ctx.assetPath;
+
+            TransferFunction tf = TransferFunctionDatabase.LoadTransferFunction(fileToImport);
+            
+            if (tf)
+            {
+                ctx.AddObjectToAsset("main obj", tf);
+                ctx.SetMainObject(tf);
+            }
+            else
+            {
+                Debug.LogError($"Failed to load transfer function: {fileToImport}");
+            }
+        }
+    }
+}
+#endif
